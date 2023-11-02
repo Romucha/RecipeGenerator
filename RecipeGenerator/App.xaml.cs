6e6b1c9e1682f73a48e8230aabb268126044e109
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-11-02 20:24:08 +0300

    Добавьте файлы проекта.

---
+++ RecipeGenerator/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeGenerator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ RecipeGenerator/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeGenerator;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
