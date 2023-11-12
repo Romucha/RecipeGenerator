70b3f2c1ef5a031d9592bad9dfad11a69dde0406
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-11-12 16:33:35 +0300

    Added basic classes for ingridients

---
+++ RecipeGenerator.API/Ingeridients/FoodAdditive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.API.Ingeridients
+{
+    public class FoodAdditive : IIngredient
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Uri Link { get; set; }
+    }
+}
---
+++ RecipeGenerator.API/Ingeridients/IIngredient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.API.Ingeridients
+{
+    /*
+        Accrording to the internet:
+        Spices
+        Dairy
+        Eggs
+        Fats
+        Flour
+        Fruits
+        Meat
+        Nuts
+        Salt
+        Sauces
+        Seafood
+        Sugar
+        Food additive
+        Oils
+        Soy sauce
+        Vinegar
+     */
+    public interface IIngredient
+    {
+        string Name { get; set; }
+
+        string Description { get; set; }
+
+        Uri Link { get; set; }
+    }
+}
