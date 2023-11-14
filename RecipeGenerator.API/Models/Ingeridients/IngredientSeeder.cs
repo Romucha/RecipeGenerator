05bc45425f5fa83cfc9f9382384422a41f0dce91
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-11-15 02:35:21 +0300

    Added seeder and resources.

---
+++ RecipeGenerator.API/Models/Ingeridients/IngredientSeeder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.API.Models.Ingeridients
+{
+    public class IngredientSeeder : IIngredientSeeder
+    {
+        private readonly IIngredientFactory factory;
+
+        public IngredientSeeder(IIngredientFactory ingredientFactory)
+        {
+            factory = ingredientFactory;
+        }
+        public IEnumerable<IIngredient> Seed()
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+            Cereal,
+            Dairy,
+            Fruit,
+            Herb,
+            Meat,
+            Nut,
+            Oilseed,
+            Other,
+            Pulse,
+            Seafood,
+            Spice,
+            Sugar,
+            SugarProduct,
+            Vegetable,
+         */
+        private IEnumerable<IIngredient> seedCereal()
+        {
+            yield return factory.Create();
+        }
+        private IEnumerable<IIngredient> seedDairy()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedFruit()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedHerb()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedMeat()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedNut()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedOilseed()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedPulse()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedSeafood()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedSpice()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedSugar()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedSugarProduct()
+        {
+            
+        }
+        private IEnumerable<IIngredient> seedVegetable()
+        {
+            
+        }
+    }
+}
--- RecipeGenerator.API/ServiceExtensions.cs
+++ RecipeGenerator.API/ServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RecipeGenerator.API.Database;
+using RecipeGenerator.API.Models.Ingeridients;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace RecipeGenerator.API
     {
         public static IServiceCollection AddDatabase(this IServiceCollection services) 
         {
+            services.AddTransient<IIngredientFactory, IngredientFactory>();
             services.AddSqlite<RecipeDbContext>("Data Source=Recipe.db;Cache=Shared");
             return services;
         }
