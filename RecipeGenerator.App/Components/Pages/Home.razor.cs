032214aa0ff442fcb6a14203c8a88ad4d46c7e68
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-11-22 02:45:06 +0300

    Restored basic functionality.

---
+++ RecipeGenerator.App/Components/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using RecipeGenerator.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.App.Components.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        private MainVM mainVM { get; set; }
+    }
+}
