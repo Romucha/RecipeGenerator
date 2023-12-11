cb2058adf67523c7f7b7ed6ff37b4af56bb74f0b
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-12-11 23:36:01 +0300

    Moved ingredient item to a separate component.

---
+++ RecipeGenerator.RazorPages/Components/Explore/IngredientItem.razor.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.AspNetCore.Components;
+using RecipeGenerator.API.Models.Ingeridients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.RazorPages.Components.Explore
+{
+    public partial class IngredientItem
+    {
+        [Parameter]
+        public IIngredient Ingredient { get; set; }
+    }
+}
