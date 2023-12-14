4b5182b1f48c76104bf174867a9855487f9c1a6a
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-12-15 02:59:33 +0300

    Added home vm.

--- RecipeGenerator.App/MauiProgram.cs
+++ RecipeGenerator.App/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using RecipeGenerator.API;
 using RecipeGenerator.App.ViewModels;
+using RecipeGenerator.RazorPages.Components.Home;
 using RecipeGenerator.RazorPages.ViewModels.About;
 using RecipeGenerator.RazorPages.ViewModels.Explore;
 using RecipeGenerator.RazorPages.ViewModels.Start;
@@ -30,6 +31,7 @@ namespace RecipeGenerator.App
    builder.Services.AddTransient<AboutVM>();
    builder.Services.AddTransient<ExploreVM>();
    builder.Services.AddTransient<StartVM>();
+   builder.Services.AddTransient<HomeVM>();
 
    return builder.Build();
   }
---
+++ RecipeGenerator.RazorPages/Components/Home/HomeVM.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RecipeGenerator.API.Database.Recipes;
+using RecipeGenerator.API.Models.Recipes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.RazorPages.Components.Home
+{
+    public class HomeVM : ObservableObject
+    {
+        private readonly IRecipeRepository recipeRepository;
+
+        private ObservableCollection<Recipe> recipes;
+
+        public ObservableCollection<Recipe> Recipes
+        {
+            get => recipes;
+            set => SetProperty(ref recipes, value);
+        }
+
+        public HomeVM(IRecipeRepository recipeRepository)
+        {
+            this.recipeRepository = recipeRepository;
+        }
+    }
+}
