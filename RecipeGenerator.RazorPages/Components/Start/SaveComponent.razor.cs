3b0dbc299c81fa1c25ba8ae8095e4dbc242f38b0
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-12-28 02:33:03 +0300

    Added fieldsets.

---
+++ RecipeGenerator.RazorPages/Components/Start/SaveComponent.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using RecipeGenerator.RazorPages.ViewModels.Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.RazorPages.Components.Start
+{
+    public partial class SaveComponent
+    {
+        [CascadingParameter]
+        public StartVM StartVM { get; set; }
+    }
+}
