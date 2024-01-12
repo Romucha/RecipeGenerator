631c532cddbfb8e489c264f397614a3530af0204
roman-krysyaev <roman-krysyaev@yandex.ru>
2024-01-12 03:01:56 +0300

    Added more tests for ingredient factory.

--- RecipeGenerator.API.Tests/Models/Ingredients/IngredientFactoryTests.cs
+++ RecipeGenerator.API.Tests/Models/Ingredients/IngredientFactoryTests.cs
@@ -1,28 +1,38 @@
 ﻿using AutoMapper;
 using Moq;
+using RecipeGenerator.API.DTO.Ingredients;
 using RecipeGenerator.API.Mapping;
 using RecipeGenerator.API.Models.Ingeridients;
+using RecipeGenerator.API.Tests.Samples;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
 namespace RecipeGenerator.API.Tests.Models.Ingredients
 {
     public class IngredientFactoryTests
     {
+        private readonly IMapper mapper;
+        private readonly IIngredientFactory ingredientFactory;
+        public IngredientFactoryTests()
+        {
+            var configuration = new MapperConfiguration(c => c.AddProfile(new MapperInitializer()));
+            mapper = configuration.CreateMapper();
+            ingredientFactory = new IngredientFactory(mapper);
+        }
+
         [Fact]
         public void Create_Normal()
         {
             //arrange
-            var configuration = new MapperConfiguration(c => c.AddProfile(new MapperInitializer()));
-            IMapper mapper = configuration.CreateMapper();
-            IIngredientFactory ingredientFactory = new IngredientFactory(mapper);
             string name = "Name";
             string description = "Description";
             Uri link = new Uri("http://uri.uri");
-            byte[] image = new byte[] { };
+            byte[] image = Array.Empty<byte>();
             IngredientType ingredientType = IngredientType.Fruits;
             //act
             var ingredient = ingredientFactory.Create(name, description, link, image, ingredientType);
@@ -33,6 +43,49 @@ namespace RecipeGenerator.API.Tests.Models.Ingredients
             Assert.Equal(link, ingredient.Link);
             Assert.Equal(image, ingredient.Image);
             Assert.Equal(ingredientType, ingredient.IngredientType);
+        }
+
+        [Fact]
+        public void CreateFromDTO_Normal()
+        {
+            //arrange
+            CreateIngredientDTO createIngredientDTO = mapper.Map<CreateIngredientDTO>(IngredientSamples.NormalIngredient);
+            //act
+            var ingredient = ingredientFactory.CreateFromDTO(createIngredientDTO);
+            //assert
+            Assert.NotNull(ingredient);
+            Assert.Equal(createIngredientDTO.Name, ingredient.Name);
+            Assert.Equal(createIngredientDTO.Description, ingredient.Description);
+            Assert.Equal(createIngredientDTO.Link, ingredient.Link);
+            Assert.Equal(createIngredientDTO.Image, ingredient.Image);
+            Assert.Equal(createIngredientDTO.IngredientType, ingredient.IngredientType);
+        }
+
+        [Fact]
+        public void CreateFromDTO_Empty()
+        {
+            //arrange
+            CreateIngredientDTO createIngredientDTO = mapper.Map<CreateIngredientDTO>(IngredientSamples.EmptyIngredient);
+            //act
+            var ingredient = ingredientFactory.CreateFromDTO(createIngredientDTO);
+            //assert
+            Assert.NotNull(ingredient);
+            Assert.Equal(createIngredientDTO.Name, ingredient.Name);
+            Assert.Equal(createIngredientDTO.Description, ingredient.Description);
+            Assert.Equal(createIngredientDTO.Link, ingredient.Link);
+            Assert.Equal(createIngredientDTO.Image, ingredient.Image);
+            Assert.Equal(createIngredientDTO.IngredientType, ingredient.IngredientType);
+        }
+
+        [Fact]
+        public void CreateFromDTO_Null()
+        {//arrange
+            CreateIngredientDTO createIngredientDTO = mapper.Map<CreateIngredientDTO>(IngredientSamples.NullIngredient);
+            //act
+            var ingredient = ingredientFactory.CreateFromDTO(createIngredientDTO);
+            //assert
+            Assert.Null(ingredient);
+
         }
     }
 }
