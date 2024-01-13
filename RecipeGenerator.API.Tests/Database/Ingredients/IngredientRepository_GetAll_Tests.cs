dda6e098e373741b4fb717486473f9319c93aba1
roman-krysyaev <roman-krysyaev@yandex.ru>
2024-01-13 23:57:39 +0300

    Added blank files for ingredient repository tests.

---
+++ RecipeGenerator.API.Tests/Database/Ingredients/IngredientRepository_GetAll_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.API.Tests.Database.Ingredients
+{
+    public partial class IngredientRepository_Tests
+    {
+    }
+}
