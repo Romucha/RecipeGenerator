7f548838592dc416097913a1fe0fe38acc4163a3
roman-krysyaev <roman-krysyaev@yandex.ru>
2024-01-17 22:33:17 +0300

    Added tests for updating ingredients.

--- RecipeGenerator.API.Tests/Database/Ingredients/IngredientRepository_Update_Tests.cs
+++ RecipeGenerator.API.Tests/Database/Ingredients/IngredientRepository_Update_Tests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Moq;
+using RecipeGenerator.API.Database.Recipes;
+using RecipeGenerator.API.DTO.Ingredients;
+using RecipeGenerator.API.DTO.Recipes;
+using RecipeGenerator.API.Tests.Samples;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,44 @@ namespace RecipeGenerator.API.Tests.Database.Ingredients
 {
     public partial class IngredientRepository_Tests
     {
+        [Fact]
+        public async Task Update_Normal()
+        {
+            //arrange
+            await recipeDbContext.Ingredients.AddAsync(IngredientSamples.NormalIngredient);
+            await recipeDbContext.SaveChangesAsync();
+            //act
+            string alteredName = Guid.NewGuid().ToString();
+            var originalingredient = IngredientSamples.NormalIngredient;
+            UpdateIngredientDTO updateIngredientDTO = mapper.Map<UpdateIngredientDTO>(originalingredient);
+            updateIngredientDTO.Name = alteredName;
+            await ingredientRepository.Update(updateIngredientDTO);
+            //assert
+            Assert.Equal(alteredName, recipeDbContext.Ingredients.Find(IngredientSamples.NormalIngredient.Id).Name);
+        }
+
+
+        [Fact]
+        public async Task Update_NonExistent()
+        {
+            // arrange
+            await recipeDbContext.Ingredients.AddRangeAsync(IngredientSamples.NormalIngredients);
+            await recipeDbContext.SaveChangesAsync();
+            //act & assert
+            string alteredName = Guid.NewGuid().ToString();
+            var updateRecipeDTO = mapper.Map<UpdateIngredientDTO>(IngredientSamples.NormalIngredient);
+            updateRecipeDTO.Name = alteredName;
+            await Assert.ThrowsAsync<NullReferenceException>(async () => await ingredientRepository.Update(updateRecipeDTO));
+        }
+
+        [Fact]
+        public async Task Update_Null()
+        {
+            //arrange
+            var updateRecipeDTO = mapper.Map<UpdateIngredientDTO>(IngredientSamples.NullIngredient);
+            //act & assert
+            await Assert.ThrowsAsync<NullReferenceException>(async () => await ingredientRepository.Update(updateRecipeDTO));
+
+        }
     }
 }
