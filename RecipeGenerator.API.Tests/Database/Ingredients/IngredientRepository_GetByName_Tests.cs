a717312857142ff4d470c0fdb60dd540a4b67b68
roman-krysyaev <roman-krysyaev@yandex.ru>
2024-01-17 19:10:35 +0300

    Added tests for getting ingredients by name.

--- RecipeGenerator.API.Tests/Database/Ingredients/IngredientRepository_GetByName_Tests.cs
+++ RecipeGenerator.API.Tests/Database/Ingredients/IngredientRepository_GetByName_Tests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RecipeGenerator.API.DTO.Ingredients;
+using RecipeGenerator.API.Tests.Samples;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,52 @@ namespace RecipeGenerator.API.Tests.Database.Ingredients
 {
     public partial class IngredientRepository_Tests
     {
+        [Fact]
+        public async Task GetByName_Normal()
+        {
+            //arrange
+            await recipeDbContext.Ingredients.AddAsync(IngredientSamples.NormalIngredient);
+            await recipeDbContext.SaveChangesAsync();
+            GetIngredientDTO getIngredientDTO = mapper.Map<GetIngredientDTO>(IngredientSamples.NormalIngredient);
+            //act
+            var recipe = await ingredientRepository.GetByName(getIngredientDTO);
+            //assert
+            Assert.NotNull(recipe);
+            Assert.Equal(IngredientSamples.NormalIngredient.Name, recipe.Name);
+        }
+
+        [Fact]
+        public async Task GetByName_Default()
+        {
+            //arrange
+            await recipeDbContext.Ingredients.AddAsync(IngredientSamples.DefaultIngredient);
+            await recipeDbContext.SaveChangesAsync();
+            GetIngredientDTO getIngredientDTO = mapper.Map<GetIngredientDTO>(IngredientSamples.DefaultIngredient);
+            //act
+            var recipe = await ingredientRepository.GetByName(getIngredientDTO);
+            //assert
+            Assert.NotNull(recipe);
+            Assert.Equal(IngredientSamples.DefaultIngredient.Name, recipe.Name);
+        }
+
+        [Fact]
+        public async Task GetByName_NonExistent()
+        {
+            //arrange
+            GetIngredientDTO getIngredientDTO = mapper.Map<GetIngredientDTO>(IngredientSamples.NormalIngredient);
+            //act
+            var recipe = await ingredientRepository.GetByName(getIngredientDTO);
+            //assert
+            Assert.Null(recipe);
+        }
+
+        [Fact]
+        public async Task GetByName_Null()
+        {
+            //arrange
+            GetIngredientDTO getIngredientDTO = mapper.Map<GetIngredientDTO>(IngredientSamples.NullIngredient);
+            //act & assert
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await ingredientRepository.GetByName(getIngredientDTO));
+        }
     }
 }
