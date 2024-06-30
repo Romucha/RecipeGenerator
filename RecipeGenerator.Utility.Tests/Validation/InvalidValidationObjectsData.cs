e75aa956a65e198d844b8965f1d0bce8ff24d91d
roman-krysyaev <krysyaev_ry@ascon.ru>
2024-06-30 15:30:31 +0300

    Added custom validator class and unit tests for it.

---
+++ RecipeGenerator.Utility.Tests/Validation/InvalidValidationObjectsData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.Utility.Tests.Validation
+{
+    public class InvalidValidationObjectsData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { ValidationObject.Invalid_Default };
+            yield return new object[] { ValidationObject.Invalid_Null! };
+
+            yield return new object[] { ValidationObject.Invalid_Description_Null };
+
+            yield return new object[] { ValidationObject.Invalid_Name_Null };
+            yield return new object[] { ValidationObject.Invalid_Name_Empty };
+
+            yield return new object[] { ValidationObject.Invalid_Coefficient_ExceedsMaximum };
+            yield return new object[] { ValidationObject.Invalid_Coefficient_Negative };
+
+            yield return new object[] { ValidationObject.Invalid_Index_ExceedsMaximum };
+            yield return new object[] { ValidationObject.Invalid_Index_Minimum };
+            yield return new object[] { ValidationObject.Invalid_Index_Negative };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ RecipeGenerator.Utility.Tests/Validation/NormalValidationObjectsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.Utility.Tests.Validation
+{
+    public class NormalValidationObjectsData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { ValidationObject.Normal };
+            yield return new object[] { ValidationObject.Normal_Description_Empty };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
