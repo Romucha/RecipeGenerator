54d5ffb018c7154a695a824e9b9625a807af02bc
Roman Krysyaev <roman-krysyaev@yandex.ru>
2024-08-11 23:31:54 +0300

    Added localization for the view of the list of ingredients.

--- RecipeGenerator.Views/List/Ingredients/ListIngredientsView.razor.cs
+++ RecipeGenerator.Views/List/Ingredients/ListIngredientsView.razor.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using RecipeGenerator.Localization.Services;
 using RecipeGenerator.ViewModels.List.Ingredients;
 using System;
 using System.Collections.Generic;
@@ -13,12 +15,22 @@ namespace RecipeGenerator.Views.List.Ingredients
         [Inject]
         public ListIngredientsViewModel ViewModel { get; set; } = default!;
 
+        [Inject]
+        public IStringLocalizer<ListIngredientsView> StringLocalizer { get; set; } = default!;
+
+        [Inject]
+        public DynamicLocalizationService DynamicLocalizationService { get; set; } = default!;
+
         protected override async Task OnInitializedAsync()
         {
             if (ViewModel != null)
             {
                 ViewModel.PropertyChanged += (sender, e) => StateHasChanged();
                 await ViewModel.GetIngredientsAsync();
+            }
+            if (DynamicLocalizationService != null) 
+            {
+                DynamicLocalizationService.PropertyChanged += (sender, e) => StateHasChanged();
             }
         }
     }
