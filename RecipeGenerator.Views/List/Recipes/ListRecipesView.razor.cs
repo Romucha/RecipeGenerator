9cfd72e8e8397962abf3684f8b3dadb1bc59e4e4
Roman Krysyaev <roman-krysyaev@yandex.ru>
2024-08-11 20:09:20 +0300

    Added localization for view of list of recipes.

--- RecipeGenerator.Views/List/Recipes/ListRecipesView.razor.cs
+++ RecipeGenerator.Views/List/Recipes/ListRecipesView.razor.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using RecipeGenerator.Localization.Services;
 using RecipeGenerator.ViewModels.List.Recipes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +16,18 @@ namespace RecipeGenerator.Views.List.Recipes
         [Inject]
         public ListRecipesViewModel ViewModel { get; set; } = default!;
 
+        [Inject]
+        public IStringLocalizer<ListRecipesView> StringLocalizer { get; set; } = default!;
+
+        [Inject]
+        public DynamicLocalizationService? DynamicLocalizationService { get; set; } = default!;
+
         protected override async Task OnInitializedAsync()
         {
+            if (DynamicLocalizationService != null)
+            {
+                DynamicLocalizationService.PropertyChanged += (sender, e) => StateHasChanged();
+            }
             if (ViewModel != null)
             {
                 ViewModel.PropertyChanged += (sender, e) => StateHasChanged();
