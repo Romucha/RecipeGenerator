d64d241f75bcb9cd6c1599e5c5fba0ab1c669f9a
Roman Krysyaev <roman-krysyaev@yandex.ru>
2024-08-18 20:40:39 +0300

    Updated unit of work.

--- RecipeGenerator.Database/UnitsOfWork/IUnitOfWork.cs
+++ RecipeGenerator.Database/UnitsOfWork/IUnitOfWork.cs
@@ -1,5 +1,4 @@
-﻿using RecipeGenerator.DTO.Interfaces.Requests;
-using RecipeGenerator.DTO.Interfaces.Responses;
+﻿using RecipeGenerator.Database.Repositories;
 using RecipeGenerator.Models;
 
 namespace RecipeGenerator.Database.UnitsOfWork
@@ -9,66 +8,13 @@ namespace RecipeGenerator.Database.UnitsOfWork
     /// </summary>
     public interface IUnitOfWork
     {
-        /// <summary>
-        /// Creates a recipe generator entity.
-        /// </summary>
-        /// <typeparam name="Entity">Recipe generator entity.</typeparam>
-        /// <param name="request">Request.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>Response.</returns>
-        Task<Response?> CreateAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : ICreateRequest
-            where Response : ICreateResponse;
+        ApplicableIngredientRepository ApplicableIngredientRepository { get; }
 
-        /// <summary>
-        /// Deletes a recipe generator entity.
-        /// </summary>
-        /// <typeparam name="Entity">Recipe generator entity.</typeparam>
-        /// <param name="request">Request.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>Response.</returns>
-        Task<Response?> DeleteAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IDeleteRequest
-            where Response : IDeleteResponse;
+        AppliedIngredientRepository AppliedIngredientRepository { get; }
 
-        /// <summary>
-        /// Gets a list of recipe generator entities.
-        /// </summary>
-        /// <typeparam name="Entity">Recipe generator entity.</typeparam>
-        /// <param name="request">Request.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>Response.</returns>
-        Task<Response?> GetAllAsync<Entity, Request, Response, ResponseItem>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IGetAllRequest
-            where Response : IGetAllResponse<IGetAllResponseItem>
-            where ResponseItem : IGetAllResponseItem;
+        RecipeRepository RecipeRepository { get; }
 
-        /// <summary>
-        /// Gets a recipe generator entity.
-        /// </summary>
-        /// <typeparam name="Entity">Recipe generator entity.</typeparam>
-        /// <param name="request">Request.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>Response.</returns>
-        Task<Response?> GetAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IGetRequest
-            where Response : IGetResponse;
-
-        /// <summary>
-        /// Updates a recipe generator entity.
-        /// </summary>
-        /// <typeparam name="Entity">Recipe generator entity.</typeparam>
-        /// <param name="request">Request.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>Response.</returns>
-        Task<Response?> UpdateAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IUpdateRequest
-            where Response : IUpdateResponse;
+        StepRepository StepRepository { get; }
 
         /// <summary>
         /// Save changes in database.
--- RecipeGenerator.Database/UnitsOfWork/UnitOfWork.cs
+++ RecipeGenerator.Database/UnitsOfWork/UnitOfWork.cs
@@ -2,13 +2,6 @@
 using Microsoft.Extensions.Logging;
 using RecipeGenerator.Database.Context;
 using RecipeGenerator.Database.Repositories;
-using RecipeGenerator.DTO.Interfaces.Requests;
-using RecipeGenerator.DTO.Interfaces.Responses;
-using RecipeGenerator.Models;
-using RecipeGenerator.Models.Ingredients;
-using RecipeGenerator.Models.Recipes;
-using RecipeGenerator.Models.Steps;
-using System.Reflection;
 
 namespace RecipeGenerator.Database.UnitsOfWork
 {
@@ -19,7 +12,10 @@ namespace RecipeGenerator.Database.UnitsOfWork
     {
         private readonly ILogger<UnitOfWork> logger;
         private readonly RecipeGeneratorDbContext dbContext;
-        private GenericDictionary repositories;
+        private readonly RecipeRepository recipeRepository;
+        private readonly StepRepository stepRepostiry;
+        private readonly AppliedIngredientRepository appliedIngredientRepository;
+        private readonly ApplicableIngredientRepository applicableIngredientRepository;
         private readonly IMapper mapper;
 
         /// <summary>
@@ -35,54 +31,19 @@ namespace RecipeGenerator.Database.UnitsOfWork
         public UnitOfWork(
             ILogger<UnitOfWork> logger,
             RecipeGeneratorDbContext dbContext,
-            IRepository<Recipe> recipeRepository,
-            IRepository<Step> stepRepostiry,
-            IRepository<AppliedIngredient> appliedIngredientRepository,
-            IRepository<ApplicableIngredient> applicableIngredientRepository, IMapper mapper)
+            RecipeRepository recipeRepository,
+            StepRepository stepRepostiry,
+            AppliedIngredientRepository appliedIngredientRepository,
+            ApplicableIngredientRepository applicableIngredientRepository, 
+            IMapper mapper)
         {
             this.logger = logger;
             this.dbContext = dbContext;
-            repositories = new();
-            repositories.Add(typeof(Recipe), recipeRepository);
-            repositories.Add(typeof(Step), stepRepostiry);
-            repositories.Add(typeof(AppliedIngredient), appliedIngredientRepository);
-            repositories.Add(typeof(ApplicableIngredient), applicableIngredientRepository);
-
+            this.recipeRepository = recipeRepository;
+            this.stepRepostiry = stepRepostiry;
+            this.appliedIngredientRepository = appliedIngredientRepository;
+            this.applicableIngredientRepository = applicableIngredientRepository;
             this.mapper = mapper;
-        }
-
-        private object? getValue(object src, string propName)
-        {
-            var prop = src.GetType().GetProperty(propName);
-            if (prop == null)
-                return default;
-
-            var value = prop.GetValue(src, null);
-
-            if (value == null)
-                return default;
-
-            return value;
-        }
-
-
-        private void setValue<T>(T obj, string propertyName, object value)
-        {
-            // these should be cached if possible
-            Type type = typeof(T);
-            PropertyInfo pi = type.GetProperty(propertyName)!;
-
-            var piType = Nullable.GetUnderlyingType(pi.PropertyType) ?? pi.PropertyType;
-
-            if (piType.IsEnum)
-            {
-                object enumValue = Enum.ToObject(piType, value);
-                pi.SetValue(obj, Convert.ChangeType(enumValue, piType), null);
-            }
-            else
-            {
-                pi.SetValue(obj, value, null);
-            }
         }
 
         /// <inheritdoc/>
@@ -98,184 +59,17 @@ namespace RecipeGenerator.Database.UnitsOfWork
                 logger.LogError(ex, nameof(SaveChangesAsync));
                 throw;
             }
-            finally
-            {
-
-            }
-        }
-
-
-        public async Task<Response?> CreateAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : ICreateRequest
-            where Response : ICreateResponse
-        {
-            try
-            {
-                logger.LogInformation($"Creating {typeof(Entity).Name}...");
-                var ingredient = await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).CreateAsync(cancellationToken);
-
-                return mapper.Map<Response>(ingredient);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, nameof(CreateAsync));
-                return default;
-            }
-            finally
-            {
-                logger.LogInformation("Done.");
-            }
-        }
-
-        public async Task<Response?> DeleteAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IDeleteRequest
-            where Response : IDeleteResponse
-        {
-            try
-            {
-                logger.LogInformation($"Deleting {typeof(Entity).Name}...");
-                var ingredient = await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).GetAsync(request.Id, cancellationToken);
-                await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).DeleteAsync(request.Id, cancellationToken);
-
-                return mapper.Map<Response>(ingredient);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, nameof(DeleteAsync));
-                return default;
-            }
-            finally
-            {
-                logger.LogInformation("Done.");
-            }
-        }
-
-        public async Task<Response?> GetAllAsync<Entity, Request, Response, ResponseItem>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IGetAllRequest
-            where Response : IGetAllResponse<IGetAllResponseItem>
-            where ResponseItem : IGetAllResponseItem
-        {
-            try
-            {
-                logger.LogInformation($"Getting {typeof(Entity).Name}s...");
-                var entites = await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).GetAllAsync(request.PageNumber, request.PageSize, request.Filter, cancellationToken);
-                List<Entity> fitleredEntities = new();
-                if (entites != null)
-                {
-                    if (Activator.CreateInstance(typeof(Response)) is not Response response)
-                        return default;
-                    response.Items = entites.Select(c => (IGetAllResponseItem)mapper.Map<ResponseItem>(c));
-                    return response;
-                }
-
-                return default;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, nameof(GetAllAsync));
-                return default;
-            }
-            finally
-            {
-                logger.LogInformation("Done.");
-            }
-        }
-
-        public async Task<Response?> GetAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IGetRequest
-            where Response : IGetResponse
-        {
-            try
-            {
-                logger.LogInformation($"Getting {typeof(Entity)}...");
-                var entity = await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).GetAsync(request.Id, cancellationToken);
-
-                return mapper.Map<Response>(entity);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, nameof(GetAsync));
-                return default;
-            }
-            finally
-            {
-                logger.LogInformation("Done.");
-            }
-        }
-
-
-        private void editEntity<Entity>(Entity request, Entity entity)
-        {
-            var requestType = request!.GetType();
-            var entityType = entity!.GetType();
-
-            foreach (var reqProp in requestType.GetProperties())
-            {
-                if (reqProp.Name != "Id")
-                {
-                    var requestPropValue = getValue(request, reqProp.Name);
-
-                    if (requestPropValue != null)
-                    {
-                        setValue(entity, reqProp.Name, requestPropValue);
-                    }
-                }
-            }
-        }
-
-        public async Task<Response?> UpdateAsync<Entity, Request, Response>(Request request, CancellationToken cancellationToken = default)
-            where Entity : IRecipeGeneratorEntity
-            where Request : IUpdateRequest
-            where Response : IUpdateResponse
-        {
-            try
-            {
-                logger.LogInformation($"Updating {typeof(Entity).Name}...");
-                var entity = await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).GetAsync(request.Id, cancellationToken);
-                if (entity == null)
-                {
-                    return default;
-                }
-
-                Entity updateStorage = mapper.Map<Entity>(request);
-
-                editEntity(updateStorage, entity);
-
-                await repositories.GetValue<IRepository<Entity>>(typeof(Entity)).UpdateAsync(entity, cancellationToken);
-
-                return mapper.Map<Response>(entity);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, nameof(UpdateAsync));
-                return default;
-            }
-            finally
-            {
-                logger.LogInformation("Done.");
-            }
-        }
-
-        private class GenericDictionary
-        {
-            private Dictionary<object, object> _dict = new Dictionary<object, object>();
-
-            public void Add<T>(object key, T value) where T : class
-            {
-                _dict.Add(key, value);
-            }
-
-            public T GetValue<T>(object key) where T : class
-            {
-                return (_dict[key] as T)!;
-            }
         }
 
         private bool disposed = false;
+
+        public ApplicableIngredientRepository ApplicableIngredientRepository => applicableIngredientRepository;
+
+        public AppliedIngredientRepository AppliedIngredientRepository => appliedIngredientRepository;
+
+        public RecipeRepository RecipeRepository => recipeRepository;
+
+        public StepRepository StepRepository => stepRepostiry;
 
         protected virtual void Dispose(bool disposing)
         {
