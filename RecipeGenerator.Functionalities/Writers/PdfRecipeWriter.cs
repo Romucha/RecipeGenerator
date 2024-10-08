305d12ba7bb9c012c630e271347e3043f0f56149
Roman Krysyaev <roman-krysyaev@yandex.ru>
2024-10-09 01:02:08 +0300

    Readjusted elements of recipe in pdf.

--- RecipeGenerator.Functionalities/Writers/PdfRecipeWriter.cs
+++ RecipeGenerator.Functionalities/Writers/PdfRecipeWriter.cs
@@ -24,18 +24,23 @@ namespace RecipeGenerator.Functionalities.Writers
             PdfDocument document = new PdfDocument();
 
             document.Info.Title = recipe.Name;
+            document.Info.Author = Environment.UserName;
+            document.Info.Subject = recipe.Name;
+            document.Info.Keywords = recipe.CourseType.ToString();
 
             PdfPage page = document.AddPage();
 
             XGraphics gfx = XGraphics.FromPdfPage(page);
 
-            AddTitle(gfx, recipe.Name, page.Width, page.Height);
-            AddEstimatedTime(gfx, recipe.EstimatedTime, page.Width, page.Height);
-            AddPortions(gfx, recipe.Portions, page.Width, page.Height);
-            AddImage(gfx, recipe.Image);
-            AddDescription(gfx, recipe.Description, page.Width, page.Height);
-            AddIngredients(gfx, ingredients);
-            AddSteps(gfx, steps);
+            XRect rect = new XRect(0, 0, page.Width.Point, page.Height.Point);
+
+            AddTitle(gfx, recipe.Name, rect);
+            AddEstimatedTime(gfx, recipe.EstimatedTime, rect);
+            AddPortions(gfx, recipe.Portions, rect);
+            AddDescription(gfx, recipe.Description, rect);
+            AddImage(gfx, recipe.Image, rect);
+            AddIngredients(gfx, ingredients, rect);
+            AddSteps(gfx, steps, rect);
 
             
 
@@ -46,47 +51,47 @@ namespace RecipeGenerator.Functionalities.Writers
             document.Save(saveFile);
         }
 
-        private void AddTitle(XGraphics xGraphics, string title, double width, double height)
+        private void AddTitle(XGraphics xGraphics, string title, XRect rect)
         {
             XFont font = new XFont("Verdana", 20, XFontStyleEx.Regular);
 
-            xGraphics.DrawString(title, font, XBrushes.Black, new XRect(new XPoint(0, 0), new XPoint(width, height)), XStringFormats.TopLeft);
+            xGraphics.DrawString(title, font, XBrushes.Black, new XPoint(30, 30));
         }
 
-        private void AddEstimatedTime(XGraphics xGraphics, TimeSpan estimatedTime, double width, double height)
+        private void AddEstimatedTime(XGraphics xGraphics, TimeSpan estimatedTime, XRect rect)
         {
             XFont font = new XFont("Verdana", 14, XFontStyleEx.Italic);
 
-            xGraphics.DrawString(estimatedTime.TotalMinutes.ToString(), font, XBrushes.Black, new XRect(new XPoint(0, 0), new XPoint(width, height)), XStringFormats.TopLeft);
+            xGraphics.DrawString($"Estimated time: {estimatedTime.TotalMinutes.ToString()}", font, XBrushes.Black, new XPoint(30, 60));
         }
 
-        private void AddPortions(XGraphics xGraphics, int portions, double width, double height)
+        private void AddPortions(XGraphics xGraphics, int portions, XRect rect)
         {
             XFont font = new XFont("Verdana", 14, XFontStyleEx.Italic);
 
-            xGraphics.DrawString(portions.ToString(), font, XBrushes.Black, new XRect(new XPoint(0, 0), new XPoint(width, height)), XStringFormats.TopLeft);
+            xGraphics.DrawString($"Portions: {portions.ToString()}", font, XBrushes.Black, new XPoint(30, 90));
         }
 
-        private void AddImage(XGraphics xGraphics, byte[] image) 
-        {
-            using MemoryStream memoryStream = new MemoryStream(image, 0, image.Length, false, true);
-            var loadedImage = XImage.FromStream(memoryStream);
-            xGraphics.DrawImage(loadedImage, 0, 0, loadedImage.PointHeight / 2, loadedImage.PointHeight / 2);
-        }
-
-        private void AddDescription(XGraphics xGraphics, string description, double width, double height)
+        private void AddDescription(XGraphics xGraphics, string description, XRect rect)
         {
             XFont font = new XFont("Verdana", 14, XFontStyleEx.Regular);
 
-            xGraphics.DrawString(description, font, XBrushes.Black, new XRect(new XPoint(0, 0), new XPoint(width, height)), XStringFormats.TopLeft);
+            xGraphics.DrawString(description, font, XBrushes.Black, new XPoint(30, 120));
         }
 
-        private void AddIngredients(XGraphics xGraphics, IEnumerable<GetAppliedIngredientResponse?> ingredients)
+        private void AddImage(XGraphics xGraphics, byte[] image, XRect rect) 
+        {
+            using MemoryStream memoryStream = new MemoryStream(image, 0, image.Length, false, true);
+            var loadedImage = XImage.FromStream(memoryStream);
+            xGraphics.DrawImage(loadedImage, 30, 150, loadedImage.PointWidth / 3, loadedImage.PointHeight / 3);
+        }
+
+        private void AddIngredients(XGraphics xGraphics, IEnumerable<GetAppliedIngredientResponse?> ingredients, XRect rect)
         {
 
         }
 
-        private void AddSteps(XGraphics xGraphics, IEnumerable<GetStepResponse?> steps)
+        private void AddSteps(XGraphics xGraphics, IEnumerable<GetStepResponse?> steps, XRect rect)
         {
 
         }
