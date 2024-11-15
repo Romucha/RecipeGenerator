caef1bd34f6bdf47a3c7c0448e0b7552020be1ef
Roman Krysyaev <roman-krysyaev@yandex.ru>
2024-11-15 22:29:15 +0300

    Added descriptions for measurement enumerations.

--- RecipeGenerator.Models/Measurements/MeasurementType.cs
+++ RecipeGenerator.Models/Measurements/MeasurementType.cs
@@ -11,9 +11,21 @@ namespace RecipeGenerator.Models.Measurements
     /// </summary>
     public enum MeasurementType
     {
+        /// <summary>
+        /// Undefined one.
+        /// </summary>
         None = 0,
+        /// <summary>
+        /// For liquids (water, milk etc.).
+        /// </summary>
         Liquid,
+        /// <summary>
+        /// For powder like substances (sugar, salt).
+        /// </summary>
         Powder,
+        /// <summary>
+        /// For pieces of ingredients (whole carrots, cabbages etc.).
+        /// </summary>
         Solid,
     }
 }
