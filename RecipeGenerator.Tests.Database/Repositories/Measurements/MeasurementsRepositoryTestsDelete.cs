e07d86987ad302776241ef5e6f8efc454c670cb9
Roman Krysyaev <roman-krysyaev@yandex.ru>
2025-01-09 00:45:51 +0300

    Added tests for deleting measurements.

---
+++ RecipeGenerator.Tests.Database/Repositories/Measurements/MeasurementsRepositoryTestsDelete.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging.Abstractions;
+using RecipeGenerator.Database.Context;
+using RecipeGenerator.Database.Repositories;
+using RecipeGenerator.Models.Measurements;
+using RecipeGenerator.Tests.Data.Database;
+using RecipeGenerator.Tests.Data.Models;
+using RecipeGenerator.Utility.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.Tests.Database.Repositories.Measurements
+{
+    public partial class MeasurementsRepositoryTests
+    {
+        [Fact]
+        public async Task Delete_Normal()
+        {
+            using (var context = await DatabaseData.ProvideDbContext().WithSingularItems())
+            {
+                var repository = GetRepository(context);
+                var expectedId = MeasurementData.Normal.Id;
+                var expectedName = MeasurementData.Normal.Name;
+
+                var response = await repository.DeleteAsync(expectedId);
+                await context.SaveChangesAsync();
+
+                Assert.NotNull(response);
+                Assert.Equal(MeasurementData.Normal.Id, response.Id);
+                Assert.Equal(MeasurementData.Normal.Name, response.Name);
+                Assert.False(context.Measurements.Any(c => c.Id == response.Id));
+            }
+        }
+
+        [Fact]
+        public async Task Delete_Default()
+        {
+            using (var context = await DatabaseData.ProvideDbContext().WithSingularItems())
+            {
+                var repository = GetRepository(context);
+                var id = 0;
+
+                var response = await repository.DeleteAsync(id);
+                await context.SaveChangesAsync();
+
+                Assert.Null(response);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_NonExistent()
+        {
+            using (var context = await DatabaseData.ProvideDbContext().WithSingularItems())
+            {
+                var repository = GetRepository(context);
+                var id = int.MaxValue;
+
+                var response = await repository.DeleteAsync(id);
+                await context.SaveChangesAsync();
+
+                Assert.Null(response);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_Negative()
+        {
+            using (var context = await DatabaseData.ProvideDbContext().WithSingularItems())
+            {
+                var repository = GetRepository(context);
+                var id = -1;
+
+                var response = await repository.DeleteAsync(id);
+                await context.SaveChangesAsync();
+
+                Assert.Null(response);
+            }
+        }
+    }
+}
