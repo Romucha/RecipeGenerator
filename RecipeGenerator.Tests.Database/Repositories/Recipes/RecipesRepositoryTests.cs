551c228478e51794d313f362c53943373dd29fe5
Roman Krysyaev <roman-krysyaev@yandex.ru>
2025-01-12 21:51:37 +0300

    Added tests for creating recipes.

---
+++ RecipeGenerator.Tests.Database/Repositories/Recipes/RecipesRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging.Abstractions;
+using RecipeGenerator.Database.Context;
+using RecipeGenerator.Database.Repositories;
+using RecipeGenerator.Utility.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.Tests.Database.Repositories.Recipes
+{
+    public partial class RecipesRepositoryTests
+    {
+        private readonly IMapper mapper;
+        public RecipesRepositoryTests()
+        {
+            mapper = new MapperConfiguration(cfg => cfg.AddProfile<MapperInitializer>()).CreateMapper();
+        }
+
+        private RecipesRepository GetRepository(RecipeGeneratorDbContext context)
+        {
+            return new RecipesRepository(new NullLogger<RecipesRepository>(), context, mapper);
+        }
+    }
+}
---
+++ RecipeGenerator.Tests.Database/Repositories/Recipes/RecipesRepositoryTestsCreate.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging.Abstractions;
+using RecipeGenerator.Database.Context;
+using RecipeGenerator.Database.Repositories;
+using RecipeGenerator.Models.Measurements;
+using RecipeGenerator.Models.Recipes;
+using RecipeGenerator.Tests.Data.Database;
+using RecipeGenerator.Tests.Data.Models;
+using RecipeGenerator.Utility.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.Tests.Database.Repositories.Recipes
+{
+    public partial class RecipesRepositoryTests
+    {
+        [Fact]
+        public async Task Create_Normal()
+        {
+            using (var context = await DatabaseData.ProvideDbContext().WithSingularItems())
+            {
+                var repository = GetRepository(context);
+
+                var response = await repository.CreateAsync();
+                await context.SaveChangesAsync();
+
+                Assert.NotNull(response);
+                Assert.NotEqual(0, response.Id);
+                Assert.NotEqual(RecipeData.Normal.Id, response.Id);
+                Assert.Equal(string.Empty, response.Name);
+                Assert.Equal(string.Empty, response.Description);
+                Assert.Equal(TimeSpan.Zero, response.EstimatedTime);
+                Assert.Equal([], response.Image);
+                Assert.Equal(Course.Unknown, (Course)response.CourseType);
+                Assert.Equal(0, response.Portions);
+            }
+        }
+    }
+}
