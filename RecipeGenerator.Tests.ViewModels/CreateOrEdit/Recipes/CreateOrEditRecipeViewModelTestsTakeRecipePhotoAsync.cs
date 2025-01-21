24c1c1ccc793c99d7c36c0fcedbc1083211acc3a
Roman Krysyaev <roman-krysyaev@yandex.ru>
2025-01-21 22:28:38 +0300

    Added tests for taking recipe photo.

---
+++ RecipeGenerator.Tests.ViewModels/CreateOrEdit/Recipes/CreateOrEditRecipeViewModelTestsTakeRecipePhotoAsync.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using RecipeGenerator.Database.Context;
+using RecipeGenerator.Database.Extenstions;
+using RecipeGenerator.Database.Repositories;
+using RecipeGenerator.Database.Seeding.ApplicableIngredients;
+using RecipeGenerator.Database.UnitsOfWork;
+using RecipeGenerator.Localization.Models.Models;
+using RecipeGenerator.Settings;
+using RecipeGenerator.Tests.Data.Database;
+using RecipeGenerator.Tests.Data.Models;
+using RecipeGenerator.Utility.Mapping;
+using RecipeGenerator.ViewModels.CreateOrEdit.Recipes;
+using RecipeGenerator.ViewModels.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeGenerator.Tests.ViewModels.CreateOrEdit.Recipes
+{
+    public partial class CreateOrEditRecipeViewModelTests
+    {
+        [Fact]
+        public async Task TakeRecipePhoto_Normal()
+        {
+            var viewModel = GetViewModel();
+            await viewModel.InitializeAsync(null);
+
+            await viewModel.TakeRecipePhotoAsync();
+
+            Assert.NotEmpty(viewModel.Image);
+            Assert.Equal(Tests.Data.Properties.Resources.RecipeNormal, viewModel.Image);
+        }
+    }
+}
